784b1f54d31bd50425c5fac0b5790c31fbcb71a1
Paul Miller <paul@citywebconsultants.co.uk>
2020-03-09 23:51:36 +0000

    Amending spelling

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const groupArrays = require('./groupArray');
+const groupArray = require('./groupArray');
 
 // Although this gives a different grouping result from the pseudo-code,
 // It could be argued that it does match the spec.
-const result = groupArrays([1,2,3,4,5], 3);
+const result = groupArray([1,2,3,4,5], 3);
 // expect: [ [ 1 ], [ 2 ], [ 3, 4, 5 ] ]
 
 console.log(result);
