c247c4cf3a20205799e74339e9ab125d6edc796d
Paul Miller <paul@citywebconsultants.co.uk>
2020-03-09 23:50:34 +0000

    Adding comment

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ const groupArrays = require('./groupArray');
 // Although this gives a different grouping result from the pseudo-code,
 // It could be argued that it does match the spec.
 const result = groupArrays([1,2,3,4,5], 3);
+// expect: [ [ 1 ], [ 2 ], [ 3, 4, 5 ] ]
 
 console.log(result);
